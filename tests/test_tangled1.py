53d2862ded808f4dbc0de762ae26af28f0cb8a27
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-05-10 17:01:11 +0430

    reran readme.org

--- tests/test_tangled1.py
+++ tests/test_tangled1.py
@@ -5,7 +5,7 @@ NI = True
 name="A$ron"
 z("echo Hello {name}")
 
-t1=""
+t1="Hello A$ron"
 def test1():
     assert t1 == "Hello A$ron"
     return True
@@ -14,11 +14,10 @@ NI or test1()
 alist = ["# Fruits", "1. Orange", "2. Rambutan", "3. Strawberry"]
 z("for i in {alist} ; do echo $i ; done")
 
-t2="""Traceback (most recent call last):
-  File \"<stdin>\", line 9, in <module>
-  File \"<string>\", line 2, in <module>
-NameError: name 'z' is not defined
-"""
+t2="""# Fruits
+1. Orange
+2. Rambutan
+3. Strawberry"""
 def test2():
     assert t2 == """# Fruits
 1. Orange
@@ -31,11 +30,7 @@ if z("test -e ~/"):
 else:
     print("We're homeless :(")
 
-t3="""Traceback (most recent call last):
-  File \"<stdin>\", line 1, in <module>
-  File \"/var/folders/5v/g3zxt_7d64g3sd_56bzpqbvh0000gn/T/babel-JVsAap/python-BtgRvV\", line 1, in <module>
-    if z(\"test -e ~/\"):
-NameError: name 'z' is not defined"""
+t3="HOME exists!"
 assert t3 == "HOME exists!"
 
 for path in z("command ls ~/tmp/"): # `command` helps bypass potential aliases defined on `ls`
