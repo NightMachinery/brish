d140af8816dce4e44efad379fcf90a78f42e2c80
NightMachinery <feraidoonmehri@gmail.com>
2024-10-14 14:53:55 +0330

    added z_background, updated docs

--- brish/__init__.py
+++ brish/__init__.py
@@ -1,19 +1,60 @@
+from concurrent.futures import ThreadPoolExecutor
 import os
 from .brishz import *
 from .brishmod import *
+
 bsh = Brish(delayed_init=True, server_count=4)
 z = bsh.z
 zp = bsh.zp
 zpe = bsh.zpe
 zq = bsh.zsh_quote
 zs = bsh.zstring
+
+
+##
+def z_background(
+    *args,
+    background_p=True,
+    getframe=1,
+    locals_=None,
+    **kwargs,
+):
+    locals_ = get_locals(
+        getframe=getframe,
+        locals_=locals_,
+    )
+
+    def run_command(locals_):
+        bsh = Brish(delayed_init=True, server_count=1)
+        return bsh.z(
+            *args,
+            locals_=locals_,
+            **kwargs,
+        )
+
+    if background_p:
+        executor = ThreadPoolExecutor(max_workers=1)
+        future = executor.submit(run_command, locals_)
+        executor.shutdown(wait=False)
+        return future
+
+    else:
+        return run_command(locals_)
+
+
+zb = z_background
+
+
 ## @personal
 def zn(*a, getframe=3, **kw):
     "Runs only if night.sh is installed"
 
     if isNight:
         return z(*a, **kw, getframe=getframe)
+
     else:
         # return None
         return CmdResult(127, "", "night.sh not found", "NA", "NA")
+
+
 ##
--- brish/brishmod.py
+++ brish/brishmod.py
@@ -67,6 +67,31 @@ class NonzeroBrishException(Exception):
     pass
 
 
+def get_locals(
+    *,
+    getframe=1,
+    locals_=None,
+):
+    getframe += 1
+    #: To adjust for this function itself
+
+    if locals_ is None:
+        try:
+            ##
+            #: @duplicateCode/c55d771bbc2dfcf7f154f78c6634b680
+            previous_frame = sys._getframe(getframe)
+            previous_frame_locals = previous_frame.f_locals
+            locals_ = dict(previous_frame.f_globals, **previous_frame_locals)
+            # https://stackoverflow.com/questions/1041639/get-a-dict-of-all-variables-currently-in-scope-and-their-values
+            # We will still miss the closure variables.
+            ##
+        except:
+            # Julia runs Python in an embedded mode with no stack frame.
+            pass
+
+    return locals_
+
+
 @dataclass(frozen=True)
 class CmdResult:
     retcode: int
@@ -446,7 +471,8 @@ class Brish:
     _conversions = {"a": ascii, "r": repr, "s": str, "e": idem, "b": boolsh}
 
     def zstring_old(self, template, locals_=None):
-        # DEPRECATED
+        #: DEPRECATED
+        ##
         if locals_ is None:
             previous_frame = sys._getframe(1)
             previous_frame_locals = previous_frame.f_locals
@@ -475,17 +501,16 @@ class Brish:
         cmd = "".join(result)
         return cmd
 
-    def zstring(self, template, locals_=None, getframe=1):
-        if locals_ is None:
-            try:
-                previous_frame = sys._getframe(getframe)
-                previous_frame_locals = previous_frame.f_locals
-                locals_ = dict(previous_frame.f_globals, **previous_frame_locals)
-                # https://stackoverflow.com/questions/1041639/get-a-dict-of-all-variables-currently-in-scope-and-their-values
-                # We will still miss the closure variables.
-            except:
-                # Julia runs Python in an embedded mode with no stack frame.
-                pass
+    def zstring(
+        self,
+        template,
+        locals_=None,
+        getframe=1,
+    ):
+        locals_ = get_locals(
+            getframe=getframe,
+            locals_=locals_,
+        )
 
         def asteval(astNode):
             if astNode is not None:
